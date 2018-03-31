272d269d72ed31d064de1790e8b8f7408f01ee6c
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-03-31 15:48:47 +0400

    Add ability to limit select fields in API get calls.

--- admin/server/api/list/get.js
+++ admin/server/api/list/get.js
@@ -15,13 +15,16 @@ module.exports = function (req, res) {
 			fields = listToArray(fields);
 		}
 		if (fields && !Array.isArray(fields)) {
-			return res.status(401).json({ error: 'fields must be undefined, a string, or an array' });
+			return res
+				.status(401)
+				.json({ error: 'fields must be undefined, a string, or an array' });
 		}
 	}
 	var filters = req.query.filters;
 	if (filters && typeof filters === 'string') {
-		try { filters = JSON.parse(req.query.filters); }
-		catch (e) { } // eslint-disable-line no-empty
+		try {
+			filters = JSON.parse(req.query.filters);
+		} catch (e) {} // eslint-disable-line no-empty
 	}
 	if (typeof filters === 'object') {
 		assign(where, req.list.addFiltersToQuery(filters));
@@ -33,48 +36,63 @@ module.exports = function (req, res) {
 	if (req.query.populate) {
 		query.populate(req.query.populate);
 	}
-	if (req.query.expandRelationshipFields && req.query.expandRelationshipFields !== 'false') {
+	if (
+		req.query.expandRelationshipFields
+		&& req.query.expandRelationshipFields !== 'false'
+	) {
 		req.list.relationshipFields.forEach(function (i) {
 			query.populate(i.path);
 		});
 	}
 	var sort = req.list.expandSort(req.query.sort);
-	async.waterfall([
-		function (next) {
-			if (!includeCount) {
-				return next(null, 0);
+	async.waterfall(
+		[
+			function (next) {
+				if (!includeCount) {
+					return next(null, 0);
+				}
+				query.count(next);
+			},
+			function (count, next) {
+				if (!includeResults) {
+					return next(null, count, []);
+				}
+				query.find();
+				query.limit(Number(req.query.limit) || 100);
+				query.skip(Number(req.query.skip) || 0);
+				if (sort.string) {
+					query.sort(sort.string);
+				}
+				if (req.query.select) {
+					query.select(req.query.select);
+				}
+				query.exec(function (err, items) {
+					next(err, count, items);
+				});
+			},
+		],
+		function (err, count, items) {
+			if (err) {
+				res.logError(
+					'admin/server/api/list/get',
+					'database error finding items',
+					err
+				);
+				return res.apiError('database error', err);
 			}
-			query.count(next);
-		},
-		function (count, next) {
-			if (!includeResults) {
-				return next(null, count, []);
-			}
-			query.find();
-			query.limit(Number(req.query.limit) || 100);
-			query.skip(Number(req.query.skip) || 0);
-			if (sort.string) {
-				query.sort(sort.string);
-			}
-			query.exec(function (err, items) {
-				next(err, count, items);
-			});
-		},
-	], function (err, count, items) {
-		if (err) {
-			res.logError('admin/server/api/list/get', 'database error finding items', err);
-			return res.apiError('database error', err);
-		}
 
-		return res.json({
-			results: includeResults
-				? items.map(function (item) {
-					return req.list.getData(item, fields, req.query.expandRelationshipFields);
-				})
-				: undefined,
-			count: includeCount
-				? count
-				: undefined,
-		});
-	});
+			return res.json({
+				results: includeResults
+					? items.map(function (item) {
+						return req.list.getData(
+								item,
+								fields,
+								req.query.expandRelationshipFields
+							);
+					  })
+					: undefined,
+				count: includeCount ? count : undefined,
+			});
+		}
+	);
 };
