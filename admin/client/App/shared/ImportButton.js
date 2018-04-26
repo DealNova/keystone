92225f34eb90a88def5fbe82457f337fe8a8918a
Manoj Singh Negi <justanothermanoj@gmail.com>
2018-04-27 00:41:37 +0530

    console flatbutton

--- admin/client/App/shared/ImportButton.js
+++ admin/client/App/shared/ImportButton.js
@@ -88,6 +88,27 @@ class ImportButton extends React.Component {
 
 	componentDidMount () {
 		console.log(FlatfileImporter, 'flatfile-csv-importer')
+		console.log(this.state.currentList)
+		// var robotImporter = new FlatfileImporter('demo-account', {
+		// 	fields: [{
+		// 		label: 'Robot Name',
+		// 		key: 'name'
+		// 	}, {
+		// 		label: 'Shield Color',
+		// 		key: 'shield-color',
+		// 		validator: /^[a-zA-Z]+$/
+		// 	}, {
+		// 		label: 'Robot Helmet Style',
+		// 		key: 'helmet-style',
+		// 	}, {
+		// 		label: 'Call Sign',
+		// 		key: 'sign',
+		// 		alternates: ['nickname', 'wave'],
+		// 		validator: /^\w{4}$/
+		// 	}],
+		// 	type: 'Robot'
+		// })
+		  
 	}
 
 	render() {
